c27daf31800da393c933d1135badd29a62a6e540
aunghtetmyatoo <aunghtetmyatoo@gmail.com>
2020-09-07 21:39:57 +0630

    header dropdown

--- src/App.js
+++ src/App.js
@@ -1,26 +1,53 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import { FormControl, Select, MenuItem } from "@material-ui/core";
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [countries, setCountries] = useState(["USA", "UK", "Myanmar"]);
+
+	useEffect(() => {
+		const getCountriesData = async () => {
+			await fetch("https://disease.sh/v3/covid-19/countries")
+				.then((response) => response.json())
+				.then((data) => {
+					const countries = data.map((country) => ({
+						name: country.country,
+						value: country.countryInfo.iso2
+					}));
+
+					setCountries(countries);
+				});
+		};
+		getCountriesData();
+	}, []);
+	return (
+		<div className="app">
+			<div className="app__header">
+				<h1>Convid-19 TRACKER</h1>
+				<FormControl className="app__dropdown">
+					<Select variant="outlined" value="abc">
+						{countries.map((country) => (
+							<MenuItem value={country.value}>{country.name}</MenuItem>
+						))}
+						{/* <MenuItem>WorldWide</MenuItem>
+						<MenuItem>WorldWide</MenuItem>
+						<MenuItem>WorldWide</MenuItem> */}
+					</Select>
+				</FormControl>
+			</div>
+
+			{/* Header */}
+			{/* Title dropdown */}
+
+			{/* InfoBox */}
+			{/* InfoBox */}
+			{/* InfoBox */}
+
+			{/* Table */}
+			{/* Graph */}
+
+			{/* Map */}
+		</div>
+	);
 }
 
 export default App;
